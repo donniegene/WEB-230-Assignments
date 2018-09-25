8abb0a5c2d90c3d8532d35ad600e506545ce4ffa
donniegene <36050767+donniegene@users.noreply.github.com>
2018-09-24 23:37:08 -0400

    Add files via upload
    Uploaded folder and file for web 230 assignment 040 dealing with Multi-Dimensional Arrays.

---
+++ series/multi-dimensional-arrays/index.php
@@ -0,0 +1,9 @@
+<?php
+
+  $food = array('Healthy'=>
+                    array('Salad', 'Vegetables', 'Pasta'),
+                'Unhealthy'=>
+                    array('Pizza', 'Ice cream'));
+
+  echo 'My favorite food is '.$food['Unhealthy'][0].'.';                  
+?>
