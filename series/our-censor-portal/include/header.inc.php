aa34a2e3d5c53a6f490230094b9e1142c0bc2db7
donniegene <36050767+donniegene@users.noreply.github.com>
2018-10-21 21:40:50 -0400

    Add files via upload
    merged web-230-prj-004-001 to master

--- series/our-censor-portal/include/header.inc.php
+++ series/our-censor-portal/include/header.inc.php
@@ -1,6 +1,7 @@
 <?php
 
 	include_once('include/config.inc.php');
+	include_once('menu.data.php');
 
 	if(!isset($pageTitle)) { //Check to see if pageName is not empty
 		$pageTitle = $companyName; //If empty set page to site name
@@ -15,14 +16,32 @@ echo '<html lang="en">';
 	echo '<head>';
 		echo '<meta charset="utf-8">';
 		echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-		echo '<title>'.$companyName.'</title>';
+		echo '<title>'.$pageTitle.'</title>';
 		echo '<link rel="stylesheet" type="text/css" href="assets/css/style.css">';
 	echo '</head>';
 
 	echo '<body>';
 			echo '<header>';
-			echo 		'<h1>'.$companyName.'</h1>';
+			echo 		'<h1>'.$pageTitle.'</h1>';
 			echo 		'<h2>On-going Training</h2>';
 		  echo '</header>';
   echo '</body>';
+?>
+
+<?php
+
+	function menuBuilder($menuItems) {
+
+		echo('<ul>');
+
+		foreach($menuItems as $key => $value) {
+
+			echo('<li><a href="' .$value['MenuLink']. '">'.$value['MenuName'].'</a></li>');
+		}
+
+		echo('</ul>');
+	}
+
+	// menuBuilder();  ** Not passing the correct number of arguments
+
 ?>
---
+++ series/our-censor-portal/internal-communications.php
@@ -0,0 +1,48 @@
+<?php
+  include_once('include/config.inc.php');
+  $pageTitle = 'Internal Communications';
+
+  require_once('include/header.inc.php');
+
+    $offset = 0; //Set the offset at 0. Need to step thru all occurances of search
+
+    if (isset($_POST['text'])) {
+      $text = $_POST['text']; //User input text
+  		$search = 'Acme'; // Set text to found to Acme in an array.
+  		$replace = 'Insights'; // Set text to replace found text to Insights in an array.
+
+      $search_length = strlen($search); //Find the length of the search text
+
+      if (!empty($text)) {
+        //Determines if there is input in each variable
+
+        while ($strpos = strpos($text, $search, $offset)) { //Continues while true
+          $offset = $strpos + $search_length; //Steps the offset past each occurence
+          $text = substr_replace($text, $replace, $strpos, $search_length);
+          /* Takes the text string and and replaces each occurence of the $search text with the
+          $replace text as determined by the strpos function in the while loop. The $search_length
+          determines how many characters to replace with the new text.
+          */
+        }
+   		//	echo $text;
+  		//Displays the new text after the replacements
+      } else {
+        echo 'Please fill in the text field.';
+      }
+    }
+
+  ?>
+
+
+  <form action="internal-communications.php" method="POST">
+
+    <textarea name="text" rows="6" cols="60"><?php echo $text; ?></textarea><br><br>
+    <input type="submit" value="Find and Replace">
+
+  </form>
+
+  <?php
+
+  	require_once('include/footer.inc.php');
+
+  ?>
