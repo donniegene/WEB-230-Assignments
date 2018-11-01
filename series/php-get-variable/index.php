780966aff5e98ce0fd12cc71f8985315321753e2
donniegene <da34773@ravens.racc.edu>
2018-11-01 16:28:24 -0400

    created folder and index.php file for web 230 assignment 070 working with the _GET variables.

---
+++ series/php-get-variable/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+      01. Explain what the following if statement is doing.
+*/
+
+     if (isset($_GET['day'])&&isset($_GET['date'])&&isset($_GET['year'])) { //Has the form been submitted
+           $day = $_GET['day'];  // Declare the variables
+           $date = $_GET['date'];
+           $year = $_GET['year'];
+           if (!empty($day)&&!empty($date)&&!empty($year)) {  // Are the fields not empty, do echo
+                 echo 'OK';
+           } else { // If fields are empty do echo
+                 echo 'Fill in all the fields.';
+          }
+      }
+
+
+?>
+
+<form action="index.php" method="GET"> <!-- send data collected to index.php page using the GET method -->
+      Day:<br><input type="text" name="day"><br> <!-- data entered here goes to day variable -->
+      Date:<br><input type="text" name="date"><br> <!-- data entered here goes to date variable -->
+      Year:<br><input type="text" name="year"><br><br> <!-- data entered here goes to year variable -->
+      <input type="submit" value="Submit">
+</form>
