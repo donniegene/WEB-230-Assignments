90151aa13efa080531bde56e1ce64d5aa6559671
donniegene <da34773@ravens.racc.edu>
2018-11-04 13:38:19 -0500

    completed the code to include the session_destroy() function

--- series/php-sessions/view.php
+++ series/php-sessions/view.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
-      echo 'Welcome, '.$_SESSION['username'].'. You are '.$_SESSION['age'].' years old!';
+      if (isset($_SESSION['username'])&&isset($_SESSION['age'])) {
+            echo 'Welcome, '.$_SESSION['username'].'. You are '.$_SESSION['age'].' years old!';
+      } else {
+            echo 'Please log in.';
+      }
 
 ?>
