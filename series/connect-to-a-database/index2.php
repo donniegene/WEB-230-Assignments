046c1da4721f7c4ed1434a6fe1c825627b9f9f47
donniegene <da34773@ravens.racc.edu>
2018-11-19 20:40:16 -0500

    Renamed original index to index2. Changed code per John Douglas, added else statemet took out or and second statement.

--- series/connect-to-a-database/index2.php
+++ series/connect-to-a-database/index2.php
@@ -2,14 +2,12 @@
 
 $conn_error = 'Could not connect.';
 $mysql_host = 'localhost';
-$mysql_user = 'don';
+$mysql_user = 'root';
 $mysql_pass  = '';
 $mysql_db = 'a_database';
 
-if (!@mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db)) {
+if (!@mysqli_connect($mysql_host, $mysql_user, $mysql_pass) || !@mysqli_select_db($mysql_db)) {
       die($conn_error);
-} else {
-      echo 'Connected';
 }
 
 ?>
