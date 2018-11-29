45e5a8715e926b7fb9f23f07e050274186d61967
donniegene <da34773@ravens.racc.edu>
2018-11-29 07:02:41 -0500

    Corrected line 12 for the $calories variable. Added the mysqli_free_result & mysqli_close lines.

--- series/mysql-select/index-116.php
+++ series/mysql-select/index-116.php
@@ -2,20 +2,24 @@
 require 'connect.inc.php'; //Connecting to the database or killing the code
 
       //Selecting the food & calories columns from the food table
-$query = "SELECT 'food', 'calories' FROM 'food' ORDER BY 'id' DESC";
+$query = "SELECT food, calories FROM food ORDER BY 'id' DESC";
 
       //Checking to see if a query will work
-if ($query_run = mysqli_query($query)) {
+if ($query_run = mysqli_query($con, $query)) {
 
       while ($query_row = mysqli_fetch_assoc($query_run)) {
             $food = $query_row['food'];
-            $calories = $query_row['$calories'];
+            $calories = $query_row['calories'];
 
             echo $food.' has '.$calories.' calories.<br>';
       }
 
+      mysqli_free_result($query_run);
+
+      mysqli_close($con);
+
 } else {
-      echo mysqli_error();
+      echo ("Error description: " . mysqli_error($con));
 }
 
 //$query_run = mysqli_query($query);
