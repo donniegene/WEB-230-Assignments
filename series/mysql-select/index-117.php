1e512a96ef1293b412708a008a69f7a9960789b4
donniegene <da34773@ravens.racc.edu>
2018-11-29 07:25:00 -0500

    Created code for web 230 assignment 117. Checking if db table is empty, if not complete the code.

--- series/mysql-select/index-117.php
+++ series/mysql-select/index-117.php
@@ -2,10 +2,10 @@
 require 'connect.inc.php'; //Connecting to the database or killing the code
 
       //Selecting the food & calories columns from the food table
-$query = "SELECT 'food', 'calories' FROM 'food' WHERE 'healthy_unhealthy' = 'h' AND 'calories' = '700' ORDER BY 'id' DESC";
+$query = "SELECT food, calories FROM food WHERE healthy_unhealthy = 'u' AND calories = 700 ORDER BY 'id' DESC";
 
       //Checking to see if a query will work
-if ($query_run = mysqli_query($query)) {
+if ($query_run = mysqli_query($con, $query)) {
 
       if (mysqli_num_rows($query_run)==NULL) { //Are the # of valid rows 0?
             echo 'No results found.';
@@ -13,14 +13,18 @@ if ($query_run = mysqli_query($query)) {
 
       while ($query_row = mysqli_fetch_assoc($query_run)) { //If there are valid results do the while
             $food = $query_row['food'];
-            $calories = $query_row['$calories'];
+            $calories = $query_row['calories'];
 
             echo $food.' has '.$calories.' calories.<br>';
             }
+            //Release data from memory
+      mysqli_free_result($query_run);
+            //Close connection with database.
+      mysqli_close($con);
       }
 
 } else {
-      echo mysqli_error();
+      echo ("Error description: " . mysqli_error($con));
 }
 
 ?>
