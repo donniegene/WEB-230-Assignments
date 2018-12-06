c6a116f97d4c0eee2268b5b877f623b13c00c08c
donniegene <da34773@ravens.racc.edu>
2018-12-05 21:40:30 -0500

    Updated index.php file after class. Added global $con to functions and adjusted remainder of code.

--- series/hit-counter/index.php
+++ series/hit-counter/index.php
@@ -2,45 +2,54 @@
 require 'connect.inc.php';
 $user_ip = $_SERVER['REMOTE_ADDR'];
 
-function ip_exists($ip) {
-      global $user_ip;
-      $query = "SELECT ip FROM hits_ip WHERE ip = '$user_ip'";
-      $query_run = mysqli_query($con, $query);
-      $mysqli_num_rows = mysqli_num_rows($query_run);
-      if (mysqli_num_rows == 0) { //If query is empty
-            return false;
-      } else if (mysqli_num_rows >= 1) { //If query comes back with a match
-            return true;
-      }
-}
-
-function ip_add ($ip) {
-      $query = "INSERT INTO hits_ip VALUES ('$ip')";
-      $query_run = mysqli_query($con, $query);
-      }
-
-function update_count() {
-      $query = "SELECT count FROM hits_count";
-      if ($query_run = mysqli_query($con, $query)) {
-            $count = mysqli_result($query_run, 0, count);
-            $count_inc = $count + 1;
+      function update_count() {
+            global $con;
+            $query = "SELECT count FROM hits_count";
+            if ($query_run = mysqli_query($con, $query)) {
+                  $count = mysqli_fetch_row($query_run);
+                  $count_inc = $count[0] + 1;
 
             $query_update = "UPDATE hits_count SET count = '$count_inc'";
-            $query_update_run = mysqli_query($query_update);
+            if($query_update_run = mysqli_query($con, $query_update)) {
+                  echo $count_inc.'<br>Count has been updated<br>';
 
-      mysqli_free_result($query_run); //Release data from memory
-
-      mysqli_close($con); //Close connection with database.
-      } else {
-            echo ("Error description: " . mysqli_error($con));
+                  mysqli_free_result($query_run); //Release data from memory
+                  mysqli_close($con); //Close connection with database.
+            }
       }
-}
+      }
 
-if (!ip_exists($user_ip)) {
-      update_count();
-      ip_add();
-}
+      function ip_add ($ip) {
+            global $con;
+            $query = "INSERT INTO hits_ip VALUES ('$ip')";
+            $query_run = mysqli_query($con, $query);
 
+            mysqli_free_result($query_run); //Release data from memory
+            mysqli_close($con); //Close connection with database.
+      }
+
+      function ip_exists($ip) {
+            global $user_ip;
+            global $con;
+            $query = "SELECT ip FROM hits_ip WHERE ip = '$ip'";
+            $query_run = mysqli_query($con, $query);
+            $query_num_rows = mysqli_num_rows($query_run); //Set $query_num_rows variable
+            if ($query_num_rows == 0) { //If query is empty
+                  return false;
+            } else if ($query_num_rows >= 1) { //If query comes back with a match
+                  return true;
+            }
+            mysqli_free_result($query_run); //Release data from memory
+            mysqli_close($con); //Close connection with database.
+      }
+
+      if (!ip_exists($user_ip)) {
+            update_count();
+            ip_add($user_ip);
+      }
+
+
+update_count();
 ?>
 
 <h1>My Page</h1>
